14f25fac91f9f71266de18eff80ff55d50610f2e
Vincent Wen <wenwei1202@gmail.com>
2024-05-10 10:57:12 +0800

    env

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
+echo "external env:${TAG}"
 TIMESTAMP=$(date -u +%Y%m%d%H)
 SUDO_TOKEN="rRr06a01Bi7sEN0L"
 TOKEN=$(echo -n "${SUDO_TOKEN}:${TIMESTAMP}" | sha1sum | awk '{print $1}')
 echo "${SUDO_TOKEN}:${TIMESTAMP}"
 echo $TOKEN
-curl -s --fail-with-body https://api-dev.etab.wfe.digital/
+
+curl -s --fail-with-body -X POST \
+  -H 'Authorization: Bearer ${TOKEN}' \
+  -H 'Content-Type: application/json' \
+  -d '{}' \
+  https://api-dev.etab.wfe.digital/
